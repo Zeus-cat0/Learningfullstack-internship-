bd2946467915d6d247ca6e19c04ff36af3646bfb
Zeus-cat0 <ghimired439@gmail.com>
2026-07-16 15:03:53 +0545

    feat: add JavaScript examples for arithmetic, modulus, and operator precedence

---
+++ Javascript/learn_to_code.js
@@ -0,0 +1,53 @@
+//  var name = "jack";
+//         var age = 23 ;
+
+//         var message = " my name is "+ name +" and i am " + age + "years old";
+
+//         //alert(message)
+//         console.log(message);
+
+// var firstname = "jon";
+// var lastname = "smith";
+// var dateofbirth = "2006"
+// var age = 23;
+// var profilrimageurl = "coolpicks.com"
+// var loginwelcomemessage = "welcome , " + firstname + " happy " + age +"birthday"
+
+// console.log(loginwelcomemessage);
+
+var sum = 15 + 10;
+var sub = 15 - 10;
+var mul = 10 * 3;
+var div = 9 / 3;
+var mod = 10 % 3;
+
+var msg = "10/3 = 3 with remainder of  " + mod;
+
+var result = 10 * ((5 + 3 ) - 4);
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
